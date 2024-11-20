22e3055332a9ddf97e8e76c38807333227416213
adrina <adrinakhanverdi1999@gmail.com>
2024-11-20 20:23:59 +0330

    add gsap in place add newsletter

--- scripts/app.js
+++ scripts/app.js
@@ -27,3 +27,57 @@ gsap.to(headerContentImag,{
     ease: "power1.out",
 
 })
+const scrollHeader=gsap.timeline({
+    scrollTrigger:{
+        trigger:".services",
+        start:"-200+top top",
+        toggleActions:"play reverse play reverse",
+
+
+    }
+
+})
+scrollHeader.to(".service" , {
+    scale:1,
+    duration:1, 
+    stagger:0.3
+})
+
+const scrollPlace = gsap.timeline({
+    scrollTrigger:{
+        trigger:".service",
+        start:"bottom top",
+        toggleActions:"play play play reverse",
+
+    }
+})
+scrollPlace.to(".place", {
+    scale:1,
+    duration:1,
+})
+
+const scrollNewsletter = gsap.timeline({
+    scrollTrigger:{
+    trigger:".newsletter",
+    start:"-300+top top",
+    toggleActions:"play play play reverse",
+    }
+})
+scrollNewsletter
+.to(".newsletter__img",{
+    x:0,
+    duration:1,
+    ease: "back.out(1.7)",
+})
+.to(".newsletter__title" ,{
+    y:0,
+    duration:.5,
+    ease: "circ.out",
+
+}, "-=.1")
+.to(".newsletter__caption",{
+    y:0,
+    duration:.5,
+    ease: "power4.out",
+
+})
